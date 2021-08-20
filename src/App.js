208900c438cbc01bee26f565d3a0c8479b9e15d4
ruka <lutca1320@gmail.com>
2021-08-20 19:21:20 +0900

    Modify Home Component & Add Icon Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
-import './MainBody.js'
-import MainBody from './MainBody.js';
+import React, { useState } from "react";
+import "./App.css";
+import "./MainBody.js";
+import MainBody from "./MainBody.js";
 
 function App() {
   const [viewHidden, setViewHidden] = useState({
@@ -11,20 +11,22 @@ function App() {
 
   return (
     <div className="App">
-      <Header setViewHidden={setViewHidden}/>
-      <MainBody homeHidden={viewHidden.home} projectsHidden={viewHidden.projects} />
+      <Header setViewHidden={setViewHidden} />
+      <MainBody
+        homeHidden={viewHidden.home}
+        projectsHidden={viewHidden.projects}
+      />
     </div>
   );
 }
 
-function Header({setViewHidden}) {
+function Header({ setViewHidden }) {
   const [header, setHeader] = useState({
     opened: false,
-    styCls: "header",
-    btnTxt: "+",
+    styCls: "header"
   });
   const [menuCls, setMenuCls] = useState("hidden");
-  
+
   const menuList = [
     {
       id: 1,
@@ -46,43 +48,53 @@ function Header({setViewHidden}) {
       id: 3,
       title: "About Me",
       viewHidden: {
-        home : true,
+        home: true,
         projects: true
       }
-    }];
+    }
+  ];
 
   function toggleHeaderOpen() {
     if (header.opened) {
       setHeader({
         opened: false,
-        styCls: "header",
-        btnTxt: "+",
+        styCls: "header"
       });
       setMenuCls("hidden");
     } else {
       setHeader({
         opened: true,
-        styCls: "header header-open",
-        btnTxt: "="
+        styCls: "header header-open"
       });
-      setTimeout(()=> {
+      setTimeout(() => {
         setMenuCls("");
       }, 120);
     }
+
+    document.querySelector(".header-button").classList.toggle("active");
   }
 
   return (
     <div className={header.styCls}>
       <div className="header-upper-container">
         <div className="header-title">ruka.dev</div>
-        <button className="header-button" onClick={toggleHeaderOpen}>{header.btnTxt}</button>
+        <button className="header-button" onClick={toggleHeaderOpen}>
+          +
+        </button>
       </div>
       <div className={"header-menu-container " + menuCls}>
         {menuList.map((menu) => {
-          return <div key={menu.id} onClick={() => {
-            setViewHidden({...menu.viewHidden});
-            toggleHeaderOpen();
-          }}>{menu.title}</div>;
+          return (
+            <div
+              key={menu.id}
+              onClick={() => {
+                setViewHidden({ ...menu.viewHidden });
+                toggleHeaderOpen();
+              }}
+            >
+              {menu.title}
+            </div>
+          );
         })}
       </div>
     </div>
--- src/MainBody.js
+++ src/MainBody.js
@@ -1,39 +1,54 @@
 import React, { useState } from "react";
-import './MainBody.css';
+import iconPath from "./IconPath.js";
+import "./MainBody.css";
 
-function MainBody({homeHidden, projectsHidden}) {
+function MainBody({ homeHidden, projectsHidden }) {
   return (
     <div className="MainBody">
-      <Home hidden={homeHidden}/>
-      <Projects hidden={projectsHidden} />
+      {homeHidden ? null : <Home hidden={homeHidden} />}
+      {projectsHidden ? null : <Projects hidden={projectsHidden} />}
     </div>
   );
 }
 
-function Home({hidden}) {
+function Home() {
   return (
-    <div className={"container " + (hidden ? "hidden" : "")}>
+    <div className="container">
       <div className="main-title">
-        <div className="row-text">
-          Hello. I'm ruka.
-        </div>
-        <div className="row-text">
-          I mainly develop <b>GAME</b>,
-        </div>
-        <div className="row-text">
-          but I'm studying <b>WEB</b> either.
-        </div>
-        <hr />
+        <div className="row-text">👋 Hi, I'm ruka,</div>
+        <div className="row-text">Game Developer & Web Developer</div>
+      </div>
+      <div className="main-title">
+        <div className="row-text">I mainly develop GAME,</div>
+        <div className="row-text">but I'm studying WEB either.</div>
+      </div>
+      <hr />
+      <div className="icon-container">
+        <Icon iconName="react" fillColor="#61DAFB" />
+        <Icon iconName="vuejs" fillColor="#4FC08D" />
       </div>
     </div>
   );
 }
 
-function Projects({hidden}) {
+function Projects() {
+  return <div>asd</div>;
+}
+
+function Icon({ iconName, fillColor }) {
   return (
-    <div className={hidden ? "hidden" : ""}>
-      asd
-    </div>
+    <svg
+      role="img"
+      viewBox={
+        "viewbox" in iconPath[iconName]
+          ? iconPath[iconName].viewbox
+          : "0 0 24 24"
+      }
+      fill={fillColor}
+      className="icon"
+    >
+      <path d={iconPath[iconName].path} />
+    </svg>
   );
 }
 
