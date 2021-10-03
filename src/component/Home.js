8843c67d10a82bec4d33a54d6938870ec1a28111
ruka <lutca1320@gmail.com>
2021-10-03 12:24:29 +0900

    Fix child key error

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,68 +1,78 @@
-import React from "react";
-import "./Home.css";
-import "./Home.scss";
-import Icon from "../module/Icon.js";
+import React from 'react';
+import './Home.css';
+import './Home.scss';
+import Icon from '../module/Icon.js';
 
 function Home() {
-  const textList = {
-    "main-title": [["👋 Hi, I'm ruka,", "Game Developer & Web Developer"]],
-    "sub-title": [
-      [
-        "This website is built with `react.js`,",
-        "which I'm currently learning."
-      ]
-    ]
-  };
+	const textList = {
+		'main-title': [["👋 Hi, I'm ruka,", 'Game Developer & Web Developer']],
+		'sub-title': [
+			[
+				'This website is built with `react.js`,',
+				"which I'm currently learning."
+			]
+		]
+	};
 
-  const cardShelfList = {
-    "WEB DEV": ["react", "vuejs", "bootstrap"],
-    "GAME DEV": ["unity", "unreal"],
-    "MAIN LANGUAGES": ["cpp", "javascript", "typescript"]
-  };
+	const cardShelfList = {
+		'WEB DEV': ['react', 'vuejs', 'bootstrap'],
+		'GAME DEV': ['unity', 'unreal'],
+		'MAIN LANGUAGES': ['cpp', 'javascript', 'typescript']
+	};
 
-  return (
-    <div className="container">
-      <div className="inner-container gradient-border appear-anim">
-        {Object.keys(textList).map((key) => {
-          return textList[key].map((text) => {
-            return <Text textCls={key} textList={text} />;
-          });
-        })}
-      </div>
-      <div className="inner-container appear-anim">
-        <div className="card-container">
-          {Object.keys(cardShelfList).map((key) => {
-            return <CardShelf title={key} iconNameList={cardShelfList[key]} />;
-          })}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="inner-container gradient-border appear-anim">
+				{Object.keys(textList).map((key) => {
+					return textList[key].map((text) => {
+						return <Text key={key} textCls={key} textList={text} />;
+					});
+				})}
+			</div>
+			<div className="inner-container appear-anim">
+				<div className="card-container">
+					{Object.keys(cardShelfList).map((key) => {
+						return (
+							<CardShelf
+								key={key}
+								title={key}
+								iconNameList={cardShelfList[key]}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 function Text({ textCls, textList }) {
-  return (
-    <div className={textCls}>
-      {textList.map((text) => {
-        return <div className="row-text">{text}</div>;
-      })}
-    </div>
-  );
+	return (
+		<div className={textCls}>
+			{textList.map((text) => {
+				return (
+					<div key={textList.findIndex((v) => v === text)} className="row-text">
+						{text}
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 function CardShelf({ title, iconNameList }) {
-  return (
-    <div className="card">
-      <div className="icon-title">
-        <div className="row-text">{title}</div>
-      </div>
-      <div className="icon-container">
-        {iconNameList.map((iconName) => {
-          return <Icon iconName={iconName} />;
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="card">
+			<div className="icon-title">
+				<div className="row-text">{title}</div>
+			</div>
+			<div className="icon-container">
+				{iconNameList.map((iconName) => {
+					return <Icon key={iconName} iconName={iconName} />;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
